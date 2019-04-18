86e8438e5aeca54ec598c05b0c652c128dc085e0
Raitis <pingy030799@gmail.com>
2019-04-18 17:58:14 +0300

    length validation

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
-	validates :phone, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :phone, presence: true, length: { minimum: 3 }
 	validates :datestamp, presence: true
 	validates :color, presence: true
 end
